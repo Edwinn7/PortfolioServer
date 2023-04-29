46966b2118fe1613af960c0c68e6ec6fbe828ca7
Edwin <edwinn-24@hotmail.com>
2023-04-28 22:55:20 -0500

    commited

--- server.js
+++ server.js
@@ -1,18 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const router = express.Router();
 const nodemailer = require("nodemailer");
-require('dotenv').config();
-
-// server used to send send emails
+const router = express.Router();
 const app = express();
+require('dotenv').config();
 app.use(cors());
 app.use(express.json());
-app.use("https://portfolioedn.herokuapp.com/", router);
+app.use("/", router);
 app.listen(process.env.PORT || 5000, () => console.log("Server Running"));
-// console.log(process.env.EMAIL);
-// console.log(process.env.PASS);
-
 const contactEmail = nodemailer.createTransport({
   service: 'gmail',
   auth: {
